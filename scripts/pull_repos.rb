bfe93868e6e25bf7a9c8b31fc0c174240dfb60ec
Oliver Günther <mail@oliverguenther.de>
2019-09-05 13:12:00 +0200

    Rename core env

--- scripts/pull_repos.rb
+++ scripts/pull_repos.rb
@@ -5,7 +5,12 @@ require 'pathname'
 require 'fileutils'
 require 'pry'
 
-core_docs = '/home/oliver/openproject/dev/help'
+core_docs = ENV['OPENPROJECT_CORE']
+raise 'Missing OPENPROJECT_CORE env' unless core_docs
+
+doc_folder = ENV['OPENPROJECT_DOC_ROOT'] || 'help'
+doc_root = File.join(core_docs, doc_folder)
+
 localizable_path = File.expand_path('../source/localizable', __dir__)
 openproject_source_dir = File.join(localizable_path, 'openproject')
 
@@ -15,9 +20,9 @@ if File.directory?(openproject_source_dir)
 end
 
 puts "Copying core to #{openproject_source_dir}"
-FileUtils.copy_entry core_docs, openproject_source_dir
+FileUtils.copy_entry doc_root, openproject_source_dir
 
-puts "Renaming index files to match directory index"
+puts 'Renaming index files to match directory index'
 Dir.glob("#{openproject_source_dir}/**/*.md").each do |path|
   target = path.gsub(/\.md$/, '.html.md')
 
