be9ccaebe605a46546dbb4516d68a9e181cf78c7
Inga Mai <inga.mai@outlook.de>
2019-11-22 10:50:59 +0100

    Scroll mobile (landing) header in and out

---
+++ webpack/javascripts/src/header_scroll.ts
@@ -0,0 +1,23 @@
+/*
+ * Hides mobile header on scroll down and shows it on scroll up
+ * by moving it in and out of the screen
+ */
+(function () {
+  const headerHeight = '55px';
+  let prevScrollPos = window.pageYOffset;
+
+  window.addEventListener('scroll', function() {
+    // Only on mobile screen sizes and when sidebar is closed
+    if (window.innerWidth >= 680 || $('.nav-wrapper').hasClass('active')) {
+      return;
+    }
+
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos >= currentScrollPos) {
+      $('.header, #landing-header-bar').css({ top: '0px'});
+    } else {
+      $('.header, #landing-header-bar').css({ top: '-' + headerHeight });
+    }
+    prevScrollPos = currentScrollPos;
+  });
+})();
