7edd3a5299d7e094a5b4701a2f71af1f6f74357b
Henriette Dinger <h.dinger@openproject.com>
2019-12-05 11:25:09 +0100

    Style cookie warning like on website

--- webpack/javascripts/src/cookie_warning.ts
+++ webpack/javascripts/src/cookie_warning.ts
@@ -3,7 +3,7 @@ $(document).ready(function(){
         if (localStorage.getItem('openProject-cookie-accepted') !== 'true') {
             $("#cookie-warning").fadeIn(200);
         }
-    }, 1500);
+    }, 1000);
     $(".cookie-warning--submit").click(function() {
         localStorage.setItem('openProject-cookie-accepted', 'true');
         $("#cookie-warning").fadeOut(200);
