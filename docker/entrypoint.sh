73e89091602c4cfcefdd8339fb2b31f430bcf27a
Oliver Günther <mail@oliverguenther.de>
2020-09-01 10:24:41 +0200

    Enable gzip for nginx

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,7 +10,24 @@ server {
     location / {
         root   /usr/share/nginx/html;
         index  index.html index.htm;
+        gzip on;
+        gzip_comp_level 6;
+        gzip_min_length 1100;
+        gzip_buffers 16 8k;
+        gzip_proxied any;
+        gzip_types
+            text/plain
+            text/css
+            text/js
+            text/xml
+            text/javascript
+            application/javascript
+            application/json
+            application/xml
+            application/rss+xml
+            image/svg+xml;
     }
+
     error_page   500 502 503 504  /50x.html;
     location = /50x.html {
         root   /usr/share/nginx/html;
